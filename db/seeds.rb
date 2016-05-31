ef4b99cb722b70a864a6cb9f5a8ff1af97aa51ea
Abdur Rehman <abdurrehmanamjad@gmail.com>
2016-05-31 19:25:28 +0500

    commented the gem required in db/seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+# ROO GEM FOR SPREADSHEETS, EXCEL FILES HANDLING
 require 'roo'
 
 # NAME OF THE EXCEL FILE WITH EXTENSION PRESENT IN THE ROOT DIRECTORY OF THE PROJECT
 FILE_NAME = '541437551.xls'
+
+# THESE ARE THE POSITIONS OF TRACKING ID CHARGE DESCRIPTION COLUMN, IT IS IMPORTANT THAT EXCEL MUST BE IN THE SAME FORMAT, OTHERWISE WE WILL HAVE TO REVISE THESE POSITIONS
 INDEX_TRACKING_ID_CHARGE_DESCRIPTION = [98, 100, 102, 104, 106]
 
 # PERMITTING THE PARAMETERS AT FEDEXBILLING ENTRY CREATION IN DATABASE FOR SECURITY REASONS
@@ -9,7 +12,10 @@ def fed_ex_billing_params(params)
   params.permit(:account_number, :inv_date, :inv_no, :cur_inv_bal, :track_no, :net_charge, :service, :serv_area_code, :rec_zip, :ship_zip)
 end
 
+# EXCEL FILE OPENNING 
 excel_file = Roo::Spreadsheet.open(FILE_NAME)
+
+# READING COLUMNS FROM EXCEL SHEET AND PLACING THEM IN KEY VALUE FORM IN OUR DESIRED KEYS
 excel_file.each_with_index(
 		account_number: 			'Bill to Account Number',
 		inv_date: 						'Invoice Date',
@@ -24,12 +30,10 @@ excel_file.each_with_index(
 		serv_area_code: 			'POD Service Area Code',
 		rec_zip: 							'Recipient Zip Code',
 		ship_zip: 						'Shipper Zip Code',
-		# sat: 									'Tracking ID Charge Description',
-		# res: 									'Tracking ID Charge Description',
 		# If there is a value in Ground Tracking ID Prefix only then append it before TrackNo field.
 		track_no_prefix: 			'Ground Tracking ID Prefix'
 	) do |row, index|
-	# Skipping the first row of the document which is header
+	# SKIPPING THE FIRST ROW OF THE DOCUMENT WHICH IS HEADER
 	unless index == 0
 		params = ActionController::Parameters.new(row)
 		@fed_ex_billing = FedExBilling.new(fed_ex_billing_params(params))
